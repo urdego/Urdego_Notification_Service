fe8da69fafdad62535069b3ecb896a4207fa5a37
ooni <gdbsrjsdn@gmail.com>
2025-02-04 19:10:09 +0900

    Feature(#4) : Reflection API 활용 메서드 자동 매핑, Socket 관련 코드 MessageType 넣어서 일괄 수정

--- src/main/java/urdego/io/urdego_notification_service/controller/NotificationSocketController.java
+++ src/main/java/urdego/io/urdego_notification_service/controller/NotificationSocketController.java
@@ -1,14 +1,19 @@
 package urdego.io.urdego_notification_service.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import urdego.io.urdego_notification_service.common.enums.MessageType;
 import urdego.io.urdego_notification_service.controller.client.GameServiceClient;
 import urdego.io.urdego_notification_service.controller.dto.request.ContentSelectReq;
 import urdego.io.urdego_notification_service.controller.dto.request.PlayerReq;
-import urdego.io.urdego_notification_service.controller.dto.response.RoomPlayersRes;
+import urdego.io.urdego_notification_service.controller.dto.request.WebSocketMessageReq;
+import urdego.io.urdego_notification_service.controller.dto.response.WebSocketMessageRes;
+import urdego.io.urdego_notification_service.controller.util.ReflectionUtil;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class NotificationSocketController {
@@ -16,31 +21,33 @@ public class NotificationSocketController {
     private final GameServiceClient gameServiceClient;
     private final SimpMessagingTemplate messagingTemplate;
 
-    // 플레이어 참여
-    @MessageMapping("/room/player/invite")
-    public void invitePlayer(PlayerReq request) {
-        RoomPlayersRes response = gameServiceClient.invitePlayer(request).getBody();
-        messagingTemplate.convertAndSend("/urdego/sub/" + response.roomId(), response);
+    @MessageMapping("/room/event")
+    public void handleRoomEvent(WebSocketMessageReq<?> request) {
+        Object response = null;
+        switch (request.messageType()) {
+            case PLAYER_JOINED -> response = gameServiceClient.invitePlayer((PlayerReq) request.payload()).getBody();
+            case PLAYER_REMOVED -> response = gameServiceClient.removePlayer((PlayerReq) request.payload()).getBody();
+            case PLAYER_READY -> response = gameServiceClient.readyPlayer((PlayerReq) request.payload()).getBody();
+            case CONTENT_SELECTED -> response = gameServiceClient.selectContent((ContentSelectReq) request.payload()).getBody();
+        }
+        sendMessage(response, request.messageType());
     }
 
-    // 플레이어 삭제
-    @MessageMapping("/room/player/remove")
-    public void removePlayer(PlayerReq request) {
-        RoomPlayersRes response = gameServiceClient.removePlayer(request).getBody();
-        messagingTemplate.convertAndSend("/urdego/sub/" + response.roomId(), response);
-    }
 
-    // 플레이어 준비
-    @MessageMapping("/room/player/ready")
-    public void readyPlayer(PlayerReq request) {
-        RoomPlayersRes response = gameServiceClient.readyPlayer(request).getBody();
-        messagingTemplate.convertAndSend("/urdego/sub/" + response.roomId(), response);
-    }
 
-    // 컨텐츠 선택
-    @MessageMapping("/room/select-content")
-    public void selectContent(ContentSelectReq request) {
-        gameServiceClient.selectContent(request);
+    private <T> void sendMessage(T response, MessageType messageType) {
+        if (response == null) {
+            log.info("빈 응답이므로 기본 메시지를 전송합니다.");
+            return;
+        }
+        try {
+            String roomId = ReflectionUtil.getRoomIdFromResponse(response);
+            messagingTemplate.convertAndSend(
+                    "/urdego/sub/" + roomId,
+                    new WebSocketMessageRes<>(messageType.name(), response)
+            );
+        } catch (IllegalArgumentException e) {
+            log.error("roomId 조회 실패: {}", e.getMessage());
+        }
     }
-
 }
