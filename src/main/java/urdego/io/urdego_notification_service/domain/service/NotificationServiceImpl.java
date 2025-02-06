b9750b40d6af43edec01c31dd354c083b5d4f2c5
Ban-gilhyeon <bbgiloo98@gmail.com>
2025-02-06 15:52:05 +0900

    Feature(#1) : rebase commit

--- src/main/java/urdego/io/urdego_notification_service/domain/service/NotificationServiceImpl.java
+++ src/main/java/urdego/io/urdego_notification_service/domain/service/NotificationServiceImpl.java
@@ -54,11 +54,14 @@ public class NotificationServiceImpl implements NotificationService {
         int index = IntStream.range(0, notifications.size())
                 .filter(i -> notifications.get(i).getNotificationId().toString().equals(request.notificationId()))
                 .findFirst().orElseThrow(() -> InvalidNotificationId.EXCEPTION);
+        log.info("index : {}", index);
 
         Notification updatedNotification = notifications.get(index);
         updatedNotification.updateReply(request.isAccepted());
-        simpMessagingTemplate.convertAndSend("/urdego/sub/notifications/" + updatedNotification.getTargetId(), updatedNotification);
+        WebSocketMessage<Notification> updateMessage = new WebSocketMessage<>(MessageType.NOTIFICATION, updatedNotification);
+        simpMessagingTemplate.convertAndSend("/urdego/sub/notifications/" + updatedNotification.getTargetId(), updateMessage);
 
+        log.info("Updated notification : {} , isAccept : {} , isRead : {} ", updatedNotification.getNotificationId(), updatedNotification.isAccepted(), updatedNotification.isRead());
         //redis에 수정사항 저장
         //TODO 수정 후 redis에 저장이 안됨..;;
         redisTemplate.opsForList().set(key,index, notifications);
