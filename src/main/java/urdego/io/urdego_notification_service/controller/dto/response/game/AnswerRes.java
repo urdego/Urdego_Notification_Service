a208452683a53efd651e6cdc4875405f445d88dc
ooni <gdbsrjsdn@gmail.com>
2025-02-19 17:18:54 +0900

    Refactor(#18) : feign 관련 dto type 수정

--- src/main/java/urdego/io/urdego_notification_service/controller/dto/response/game/AnswerRes.java
+++ src/main/java/urdego/io/urdego_notification_service/controller/dto/response/game/AnswerRes.java
@@ -3,7 +3,7 @@ package urdego.io.urdego_notification_service.controller.dto.response.game;
 public record AnswerRes(
         String roomId,
         String questionId,
-        String userId,
+        Long userId,
         double latitude,
         double longitude
 ) {
