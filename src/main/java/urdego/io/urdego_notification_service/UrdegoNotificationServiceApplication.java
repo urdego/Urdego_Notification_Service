bf8d5c962ad57000316047cc7c22468c0abe07a3
JoungWoo Lee <j_ra1n@naver.com>
2025-04-07 17:21:14 +0900

    Refactor: Dockerfile 수정

--- src/main/java/urdego/io/urdego_notification_service/UrdegoNotificationServiceApplication.java
+++ src/main/java/urdego/io/urdego_notification_service/UrdegoNotificationServiceApplication.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 public class UrdegoNotificationServiceApplication {
 
-    
     public static void main(String[] args) {
         SpringApplication.run(UrdegoNotificationServiceApplication.class, args);
     }
